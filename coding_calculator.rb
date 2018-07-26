cbb0af048a7c21e951f56748cc01ad4bd7a6bffe
Ly Nguyen <lnguyengwc@gmail.com>
2018-07-26 20:16:54 +0000

    automatically backed up by learn

--- coding_calculator.rb
+++ coding_calculator.rb
@@ -3,7 +3,7 @@ puts "Hello! Do you want to add, subtract, multiply or divide?"
 $operation = gets.strip
 
 
-if $operation = add
+if $operation = "add"
   puts "Pick a number."
   $number1 = gets.string
   puts "Pick another number."
@@ -11,13 +11,13 @@ if $operation = add
   $new_number = $number1 + $number2
   puts "Your answer is #{$new_number}."
   
-elsif $operation = subtract
+elsif $operation = "subtract"
   puts "Pick a number."
   
-elsif $operation = multiply
+elsif $operation = "multiply"
   puts "Pick a number."
 
-else $operation = divide
+else $operation = "divide"
   puts "Pick a number."
 
 end
