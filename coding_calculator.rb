d30a4c00b0a0932750f2758bb1432de445fd9454
Ly Nguyen <lnguyengwc@gmail.com>
2018-07-26 20:16:12 +0000

    automatically backed up by learn

--- coding_calculator.rb
+++ coding_calculator.rb
@@ -6,7 +6,10 @@ $operation = gets.strip
 if $operation = add
   puts "Pick a number."
   $number1 = gets.string
-  
+  puts "Pick another number."
+  $number2 = gets.string
+  $new_number = $number1 + $number2
+  puts "Your answer is #{$new_number}."
   
 elsif $operation = subtract
   puts "Pick a number."
