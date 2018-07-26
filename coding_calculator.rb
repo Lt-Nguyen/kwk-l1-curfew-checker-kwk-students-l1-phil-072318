dac79003ab475b4f2a31f29a95bfc1a0387f0a6d
Ly Nguyen <lnguyengwc@gmail.com>
2018-07-26 20:14:54 +0000

    automatically backed up by learn

---
+++ coding_calculator.rb
@@ -0,0 +1,20 @@
+puts "Hello! Do you want to add, subtract, multiply or divide?"
+
+$operation = gets.strip
+
+
+if $operation = add
+  puts "Pick a number."
+  $number1 = gets.string
+  
+  
+elsif $operation = subtract
+  puts "Pick a number."
+  
+elsif $operation = multiply
+  puts "Pick a number."
+
+else $operation = divide
+  puts "Pick a number."
+
+end
